0c7c9d2ecb88339ee5451ce310daa1638c0cdb4c
Guillermo <llguillermo.floresll@gmail.com>
2021-09-19 18:53:53 -0400

    Borrado de archivos y lineas innecesarias

--- docente.php
+++ docente.php
@@ -26,12 +26,10 @@ require('layout/menu.php');
 $seleccionTablaMaterias = new Crud("materias");
 //  Pasamos los datos a la funcion, que nos devuelve un array con los datos que pedimos
 $whereAndWhere = $seleccionTablaMaterias->where("idDocente","=",2)->where("idCarrera","=",1)->get();
-
 //  Muestra el array con su tipo de datos
 echo "<pre>";
 var_dump($whereAndWhere);
 echo "</pre>";
-
 //  Recorremos todos los datos devueltos por la consulta y los guardamos en variables separadas
 foreach($whereAndWhere as $index){
     $idMaterias = $index['idMaterias'];
@@ -48,27 +46,16 @@ echo "<pre>";
 var_dump($selectAll);
 echo "</pre>";
 
-
 /***************** PARA ACTUALIZAR UNA TABLA ******************/
 $update = $seleccionTablaMaterias->where("idMaterias","=",1)->update(["materia"=>"BD","idDocente"=>1,"idCarrera"=>1]);
-
 
 /***************** PARA ELIMINAR DATOS DE UNA TABLA ******************/
 $delete = $seleccionTablaMaterias->where("idMaterias","=",4)->delete();
 
-
-
-
-
-
+/***************** Muestra los registros afectados ******************/
 echo "FILAS ACTUALIZADAS: " . $update . " ELIMINADOS: " . $delete;
 
-
-
-
-
 ?>
-
 <div class="">		
 				<h2>Mis Actividades - <?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES); ?></h2>
 				<?php
@@ -79,7 +66,6 @@ echo "FILAS ACTUALIZADAS: " . $update . " ELIMINADOS: " . $delete;
 		</div>
 	</div>
 </div>
-
 <?php 
 //include header template
 require('layout/footer.php'); 
